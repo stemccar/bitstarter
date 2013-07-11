ad61715bfbe0cf113beab2c808b4c72b3dae85ec
Ubuntu <ubuntu@ip-10-196-215-216.ec2.internal>
2013-07-11 00:36:52 +0000

    reverted to original

--- web.js
+++ web.js
@@ -1,47 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
+var express = require('express');
 
-<meta charset-"utf-8">
-<title>My First Bitstarter</title>
-<link type="text/css" rel="stylesheet"
-	    href="https://d369qusza40orc.cloudfront.net/startup%2Fcode%2Fbootstrap-2.3.3.css">
-<style type="text/css">
+var app = express.createServer(express.logger());
 
-body{
-padding-top: 20px;
-padding-bottom: 40px;
-}
 
-.container{
-width: 960px;
-}
+var fs = require('fs');
+var str = fs.readFileSync('index.html');
 
-p.lead{
-padding-top: 15px;
-}
 
-.navigation, .pitch, .section1, .section2, .faq, .footer{
-padding: 10px 0px 10px 0px;
-}
+app.get('/', function(request, response) {
+ response.send(str);
+});
 
-.video, .thermometer, .order, .social{
-border: 1px dotted;
-text-align: center;
-}
-
-.video{
-height: 124px;
-line-height: 124px;
-}
-
-.thermometer, .order, .social{
-height: 40px;
-line-height: 40px;
-}
-
-div.row{
-border: 1px solid;
-}
-</style>
-</head>
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
