e43cc17607eb922cad26c194ae5bc4dd6f9d856c
Ubuntu <ubuntu@ip-10-112-39-39.ec2.internal>
2013-07-12 03:02:09 +0000

    it works

--- test/grader.js
+++ test/grader.js
@@ -1,9 +1,29 @@
 #!/usr/bin/env node
+/*
+Automatically grade files for the presence of specified HTML tags/attributes.
+Uses commander.js and cheerio. Teaches command line application development
+and basic DOM parsing.
+
+References:
+
+ + cheerio
+   - https://github.com/MatthewMueller/cheerio
+   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+   - http://maxogden.com/scraping-with-node.html
+
+ + commander.js
+   - https://github.com/visionmedia/commander.js
+   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+
+ + JSON
+   - http://en.wikipedia.org/wiki/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+*/
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -41,49 +61,14 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
-if(require.main == module) 
-{
-    
+if(require.main == module) {
     program
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        //.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .parse(process.argv);
-	var url = process.argv;
- 
-
-    	if(url.length > 2) 
-	{
-          url = url.slice(4, url.length);
-        
-	}
-        
-        rest.get(url.toString()).on('complete', function(result)
-        {
-          if (result instanceof Error) 
-            {
-               console.log("oops");
-               
-            } 
-         else 
-          {
-             var str = result;
-             fs.writeFile("test.txt", "str", function(err) 
-             {
-                if(err) 
-                 {
-                   console.log(err);
-                 } 
-                else 
-                 {
-                   console.log("The file was saved!");
-                   console.log(str);
-                   var checkJson = checkHtmlFile("test.txt", program.checks);
-                   var outJson = JSON.stringify(checkJson, null, 4);
-                   console.log(outJson);
-            
-                 }
-             }); 
-
-          }
-       });
+    var checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+} else {
+    exports.checkHtmlFile = checkHtmlFile;
 }
